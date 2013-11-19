60a8a6bf38b597fb7c58f91f7ea862f2fdad9c57
Eric Hicks <ehicks05@yahoo.com>
2013-11-19 15:04:12 +0000

    swapped width and height values...oops

--- src/hicks/DeadTVConfig.java
+++ src/hicks/DeadTVConfig.java
@@ -2,7 +2,7 @@ package hicks;
 
 public class DeadTVConfig
 {
-    public static int height = 800;
-    public static int width  = 600;
+    public static int width  = 800;
+    public static int height = 600;
     public static int delay  = 16;
 }
