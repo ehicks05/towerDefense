2093059424f6452d2aab1cf80b6192f401c3ff09
Eric Hicks <ehicks05@yahoo.com>
2014-01-19 18:34:17 +0000

    cleaned up SoundManager
    Former-commit-id: da4b74dc6dfd12aff996c4be7551d1f1ab9b1f6b

--- src/hicks/td/SoundManager.java
+++ src/hicks/td/SoundManager.java
@@ -1,75 +1,15 @@
 package hicks.td;
 
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-
 import javax.sound.sampled.*;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
 
 public class SoundManager
 {
-    private static Mixer mixer;
-    private static AudioFormat format = new AudioFormat(44100, 16, 2, false, true);
-    private static Line.Info targetLineInfo = new DataLine.Info(TargetDataLine.class, format);
-
-    private static Clip bowFire;
-    private static Clip bowHit;
-
-    private static AudioInputStream bowFireStream;
-    private static AudioInputStream bowHitStream;
-
     public static void init()
     {
-
         try
         {
-//            Mixer.Info[] mixerInfo = AudioSystem.getMixerInfo();
-//            Mixer mixer = AudioSystem.getMixer(mixerInfo[0]);
-//            mixer.open();
-
             new MediaThread().start();
-
-
-//            Clip clip = AudioSystem.getClip();
-//            clip.open(soundtrack);
-//            clip.setFramePosition(0);
-//            clip.start();
-//            bowFireStream = AudioSystem.getAudioInputStream(new File("ass\\bowFire.wav"));
-//            bowHitStream  = AudioSystem.getAudioInputStream(new File("ass\\bowHit.wav"));
-//            bowFire = AudioSystem.getClip();
-//            bowFire.open(bowFireStream);
-//            bowHit = AudioSystem.getClip();
-//            bowHit.open(bowHitStream);
-
-        }
-        catch (Exception e)
-        {
-            Log.info("sound error");
-        }
-    }
-
-    public static void playShootSFX()
-    {
-        try
-        {
-            bowFireStream = AudioSystem.getAudioInputStream(new File("ass\\bowFire.wav"));
-
-            Clip clip = AudioSystem.getClip();
-
-            clip.open(bowFireStream);
-
-            FloatControl masterGain = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-            masterGain.setValue(masterGain.getValue() - 10f);
-
-            clip.stop();
-            clip.setFramePosition(0);
-            clip.start();
-
-//            bowFire.stop();
-//            bowFire.setFramePosition(0);
-//            bowFire.start();
         }
         catch (Exception e)
         {
@@ -77,32 +17,32 @@ public class SoundManager
         }
     }
 
+    public static void playShootSFX()
+    {
+        playSound(new File("ass\\bowFire.wav"), -10f);
+    }
+
     public static void playHitSFX()
+    {
+        playSound(new File("ass\\bowHit.wav"), -10f);
+    }
+
+    private static void playSound(File soundFile, float gainAdjustment)
     {
         try
         {
-            bowHitStream = AudioSystem.getAudioInputStream(new File("ass\\bowHit.wav"));
             Clip clip = AudioSystem.getClip();
-
-            clip.open(bowHitStream);
+            clip.open(AudioSystem.getAudioInputStream(soundFile));
 
             FloatControl masterGain = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-            masterGain.setValue(masterGain.getValue() - 10f);
+            masterGain.setValue(masterGain.getValue() + gainAdjustment);
 
-            clip.stop();
             clip.setFramePosition(0);
             clip.start();
-//            bowFire.stop();
-//            bowFire.setFramePosition(0);
-//            bowFire.start();
         }
         catch (Exception e)
         {
-
+            Log.info(e.getMessage());
         }
-
-//        bowHit.stop();
-//        bowHit.setFramePosition(0);
-//        bowHit.start();
     }
 }
